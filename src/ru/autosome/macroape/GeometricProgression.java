c3bd6c00e16087f1e011b39774f7aa43007a5f44
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-10-01 17:57:11 +0400

    progressions

---
+++ src/ru/autosome/macroape/GeometricProgression.java
@@ -0,0 +1,22 @@
+package ru.autosome.macroape;
+
+import java.util.ArrayList;
+
+public class GeometricProgression extends Progression {
+  double from;
+  double to;
+  double step ;
+  double[] values() {
+    ArrayList<Double> results = new ArrayList<Double>();
+    for (double x = from; x <= to; x *= step) {
+      results.add(x);
+    }
+    return ArrayExtensions.toPrimitiveArray(results);
+  }
+
+  GeometricProgression(double min, double to, double step) {
+    this.from = min;
+    this.to = to;
+    this.step = step;
+  }
+}
---
+++ src/ru/autosome/macroape/Progression.java
@@ -0,0 +1,24 @@
+package ru.autosome.macroape;
+
+
+import java.util.StringTokenizer;
+
+abstract public class Progression {
+  abstract double[] values();
+
+  static Progression fromString(String s) {
+    StringTokenizer parser = new StringTokenizer(s);
+    double min = Double.valueOf(parser.nextToken(","));
+    double max = Double.valueOf(parser.nextToken(","));
+    double step = Double.valueOf(parser.nextToken(","));
+    String progression_method = parser.nextToken();
+
+    if (progression_method.equals("mul")) {
+      return new GeometricProgression(min, max, step);
+    } else if (progression_method.equals("add")) {
+      return new ArithmeticProgression(min, max, step);
+    } else {
+      throw new IllegalArgumentException("Progression method for pvalue-list is either add or mul, but you specified " + progression_method);
+    }
+  }
+}
