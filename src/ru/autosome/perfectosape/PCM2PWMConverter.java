4f494f182672e773aa45f7992fedc2da16da2238
Ilya Vorontsov <prijutme4ty@gmail.com>
2013-10-26 11:34:36 +0400

    PCM count and pseudount in converter are calculated columnwise now

--- src/ru/autosome/perfectosape/PCM2PWMConverter.java
+++ src/ru/autosome/perfectosape/PCM2PWMConverter.java
@@ -3,22 +3,35 @@ package ru.autosome.perfectosape;
 // TODO: extract interface for converters
 class PCM2PWMConverter {
   public BackgroundModel background;
-  private final double pseudocount;
+  private final Double const_pseudocount;
   private final PCM pcm;
 
   public PCM2PWMConverter(PCM pcm) {
     this.pcm = pcm;
     this.background = new WordwiseBackground();
-    this.pseudocount = Math.log(pcm.count());
+    this.const_pseudocount = null; // to be calculated automatically as logarithm of count
+  }
+  public PCM2PWMConverter(PCM pcm, double pseudocount) {
+    this.pcm = pcm;
+    this.background = new WordwiseBackground();
+    this.const_pseudocount = pseudocount;
   }
 
   public PWM convert() {
     double new_matrix[][] = new double[pcm.matrix.length][];
     for (int pos = 0; pos < pcm.matrix.length; ++pos) {
-      new_matrix[pos] = new double[4];
-      for (int letter = 0; letter < 4; ++letter) {
+      new_matrix[pos] = new double[PCM.ALPHABET_SIZE];
+
+      // columns can have different counts for some PCMs
+      double count = 0.0;
+      for(double element: pcm.matrix[pos]) {
+        count += element;
+      }
+      double pseudocount = (const_pseudocount != null) ? const_pseudocount : Math.log(count);
+
+      for (int letter = 0; letter < PCM.ALPHABET_SIZE; ++letter) {
         double numerator = pcm.matrix[pos][letter] + background.probability(letter) * pseudocount;
-        double denominator = background.probability(letter) * (pcm.count() + pseudocount);
+        double denominator = background.probability(letter) * (count + pseudocount);
         new_matrix[pos][letter] = Math.log(numerator / denominator);
       }
     }
