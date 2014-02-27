05bf8ad358d4a93563c6ddcf609dfb72f5749d6f
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-02-27 23:00:19 +0400

    override annotations

--- src/main/java/ru/autosome/perfectosape/backgroundModels/DiWordwiseBackground.java
+++ src/main/java/ru/autosome/perfectosape/backgroundModels/DiWordwiseBackground.java
@@ -4,6 +4,7 @@ public class DiWordwiseBackground implements BackgroundModel {
   public DiWordwiseBackground() {
   }
 
+  @Override
   public double[] probability() {
     double[] result;
     result = new double[]{0.25, 0.25, 0.25, 0.25,
@@ -13,10 +14,12 @@ public class DiWordwiseBackground implements BackgroundModel {
     return result;
   }
 
+  @Override
   public double probability(int index) {
     return 0.25;
   }
 
+  @Override
   public double[] count() {
     double[] result;
     result = new double[]{1.0, 1.0, 1.0, 1.0,
@@ -26,22 +29,27 @@ public class DiWordwiseBackground implements BackgroundModel {
     return result;
   }
 
+  @Override
   public double count(int index) {
     return 1.0;
   }
 
+  @Override
   public double volume() {
     return 4;
   }
 
+  @Override
   public String toString() {
     return "1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1";
   }
 
+  @Override
   public boolean is_wordwise() {
     return true;
   }
 
+  @Override
   public double mean_value(double[] values) {
     double sum = 0;
     for (int letter = 0; letter < 16; ++letter) {
@@ -50,6 +58,7 @@ public class DiWordwiseBackground implements BackgroundModel {
     return sum / 16.0;
   }
 
+  @Override
   public double mean_square_value(double[] values) {
     double sum_square = 0.0;
     for (int letter = 0; letter < 16; ++letter) {
