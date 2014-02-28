3d293260c234297504626fe942d73baff8c045c7
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-02-28 16:03:31 +0400

    background model and di-background model were generalized

--- src/main/java/ru/autosome/perfectosape/backgroundModels/BackgroundModel.java
+++ src/main/java/ru/autosome/perfectosape/backgroundModels/BackgroundModel.java
@@ -1,23 +1,5 @@
 package ru.autosome.perfectosape.backgroundModels;
 
-public interface BackgroundModel {
+public interface BackgroundModel extends GeneralizedBackgroundModel {
   static final int ALPHABET_SIZE = 4;
-  public double[] probability();
-
-  public double probability(int index);
-
-  public double[] count();
-
-  public double count(int index);
-
-  public double volume(); // 1 for probability model, 4 for wordwise model
-
-  @Override
-  public String toString();
-
-  public boolean is_wordwise();
-
-  public double mean_value(double[] values);
-
-  public double mean_square_value(double[] values);
 }
