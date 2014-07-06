87b99100b7b5f41111045dd966b168a07e36acc7
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-07-06 14:53:34 +0400

    generalize EvalSimilarity

--- src/main/java/ru/autosome/perfectosape/cli/EvalSimilarity.java
+++ src/main/java/ru/autosome/perfectosape/cli/EvalSimilarity.java
@@ -19,55 +19,14 @@ import ru.autosome.perfectosape.motifModels.PWM;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class EvalSimilarity {
-  private static final String DOC =
-   "Command-line format:\n" +
-    "java ru.autosome.perfectosape.cli.EvalSimilarity <1st matrix pat-file> <2nd matrix pat-file> [options]\n" +
-    "\n" +
-    "Options:\n" +
-    "  [-p <P-value>]\n" +
-    "  [-d <discretization level>]\n" +
-    "  [--pcm] - treat the input file as Position Count Matrix. PCM-to-PWM transformation to be done internally.\n" +
-    "  [--ppm] or [--pfm] - treat the input file as Position Frequency Matrix. PPM-to-PWM transformation to be done internally.\n" +
-    "  [--effective-count <count>] - effective samples set size for PPM-to-PWM conversion (default: 100). \n" +
-    "  [--boundary lower|upper] Upper boundary (default) means that the obtained P-value is greater than or equal to the requested P-value\n" +
-    "  [-b <background probabilities] ACGT - 4 numbers, comma-delimited(spaces not allowed), sum should be equal to 1, like 0.25,0.24,0.26,0.25\n" +
-    "  [--first-threshold <threshold for the first matrix>]\n" +
-    "  [--second-threshold <threshold for the second matrix>]\n" +
-    "\n" +
-    "Examples:\n" +
-    "  java ru.autosome.perfectosape.cli.EvalSimilarity motifs/KLF4_f2.pat motifs/SP1_f1.pat -p 0.0005 -d 100 -b 0.3,0.2,0.2,0.3\n";
-
-  private BackgroundModel firstBackground, secondBackground;
-  private Double discretization;
-  private double pvalue;
-  private BoundaryType pvalueBoundary;
-  private String firstPMFilename, secondPMFilename;
-  private DataModel dataModelFirst, dataModelSecond;
-
-  private Integer maxHashSize;
-  private Integer maxPairHashSize;
-
-  private Double effectiveCountFirst, effectiveCountSecond;
-
-  private Double predefinedFirstThreshold, predefinedSecondThreshold;
-  private PWM firstPWM, secondPWM;
-
-  private Double cacheFirstThreshold, cacheSecondThreshold;
-
-
-  private void extract_first_pm_filename(ArrayList<String> argv) {
-    if (argv.isEmpty()) {
-      throw new IllegalArgumentException("No input. You should specify input file");
-    }
-    firstPMFilename = argv.remove(0);
+public class EvalSimilarity extends EvalSimilarityGeneralized<PWM, BackgroundModel> {
+  @Override
+  protected String DOC_background_option() {
+    return "ACGT - 4 numbers, comma-delimited(spaces not allowed), sum should be equal to 1, like 0.25,0.24,0.26,0.25";
   }
-
-  private void extract_second_pm_filename(ArrayList<String> argv) {
-    if (argv.isEmpty()) {
-      throw new IllegalArgumentException("No input. You should specify input file");
-    }
-    secondPMFilename = argv.remove(0);
+  @Override
+  protected String DOC_run_string() {
+    return "java ru.autosome.perfectosape.cli.FindPvalue";
   }
 
   private void initialize_defaults() {
@@ -85,57 +44,18 @@ public class EvalSimilarity {
     pvalueBoundary = BoundaryType.UPPER;
   }
 
-  private void extract_option(ArrayList<String> argv) {
-    String opt = argv.remove(0);
-    if (opt.equals("-b")) {
-      BackgroundModel background = Background.fromString(argv.remove(0));
-      firstBackground = background;
-      secondBackground = background;
-    } else if (opt.equals("-p")) {
-      pvalue = Double.valueOf(argv.remove(0));
-    } else if (opt.equals("-b1")) {
-      firstBackground = Background.fromString(argv.remove(0));
-    } else if (opt.equals("-b2")) {
-      secondBackground = Background.fromString(argv.remove(0));
-    } else if (opt.equals("--max-hash-size")) {
-      maxHashSize = Integer.valueOf(argv.remove(0));
-    } else if (opt.equals("--max-2d-hash-size")) {
-      maxPairHashSize = Integer.valueOf(argv.remove(0));
-    } else if (opt.equals("-d")) {
-      discretization = Double.valueOf(argv.remove(0));
-    } else if (opt.equals("--boundary")) {
-      pvalueBoundary = BoundaryType.valueOf(argv.remove(0).toUpperCase());
-    } else if (opt.equals("--pcm")) {
-      dataModelFirst = DataModel.PCM;
-      dataModelSecond = DataModel.PCM;
-    } else if (opt.equals("--ppm") || opt.equals("--pfm")) {
-      dataModelFirst = DataModel.PPM;
-      dataModelSecond = DataModel.PPM;
-    } else if (opt.equals("--effective-count")) {
-      Double effectiveCount = Double.valueOf(argv.remove(0));
-      effectiveCountFirst = effectiveCount;
-      effectiveCountSecond = effectiveCount;
-    } else if (opt.equals("--first-threshold")) {
-      predefinedFirstThreshold = Double.valueOf(argv.remove(0));
-    } else if (opt.equals("--second-threshold")) {
-      predefinedSecondThreshold = Double.valueOf(argv.remove(0));
-    } else {
-      throw new IllegalArgumentException("Unknown option '" + opt + "'");
-    }
+  @Override
+  protected BackgroundModel extract_background(String str) {
+    return Background.fromString(str);
   }
 
-  void setup_from_arglist(ArrayList<String> argv) {
-    extract_first_pm_filename(argv);
-    extract_second_pm_filename(argv);
-    while (argv.size() > 0) {
-      extract_option(argv);
-    }
-    firstPWM = new PWMImporter(firstBackground,
-                               dataModelFirst,
-                               effectiveCountFirst).loadPWMFromParser(PMParser.from_file_or_stdin(firstPMFilename));
-    secondPWM = new PWMImporter(secondBackground,
-                                dataModelSecond,
-                                effectiveCountSecond).loadPWMFromParser(PMParser.from_file_or_stdin(secondPMFilename));
+  @Override
+  protected PWMImporter firstMotifImporter(){
+    return new PWMImporter(firstBackground, dataModelFirst, effectiveCountFirst);
+  }
+  @Override
+  protected PWMImporter secondMotifImporter(){
+    return new PWMImporter(secondBackground, dataModelSecond, effectiveCountSecond);
   }
 
   private EvalSimilarity() {
@@ -144,7 +64,7 @@ public class EvalSimilarity {
 
   private static EvalSimilarity from_arglist(ArrayList<String> argv) {
     EvalSimilarity result = new EvalSimilarity();
-    ru.autosome.perfectosape.cli.Helper.print_help_if_requested(argv, DOC);
+    ru.autosome.perfectosape.cli.Helper.print_help_if_requested(argv, result.documentString());
     result.setup_from_arglist(argv);
     return result;
   }
@@ -164,76 +84,9 @@ public class EvalSimilarity {
     return result;
   }
 
-  OutputInformation report_table_layout() {
-    OutputInformation infos = new OutputInformation();
 
-    infos.add_parameter("V", "discretization", discretization);
-    if (predefinedFirstThreshold == null || predefinedSecondThreshold == null) {
-      infos.add_parameter("P", "requested P-value", pvalue);
-    }
-    if (predefinedFirstThreshold != null) {
-      infos.add_parameter("T1", "threshold for the 1st matrix", predefinedFirstThreshold);
-    }
-    if (predefinedSecondThreshold != null) {
-      infos.add_parameter("T2", "threshold for the 2nd matrix", predefinedSecondThreshold);
-    }
-    infos.add_parameter("PB", "P-value boundary", pvalueBoundary);
-    if (firstBackground.equals(secondBackground)) {
-      infos.background_parameter("B", "background", firstBackground);
-    } else {
-      infos.background_parameter("B1", "background for the 1st model", firstBackground);
-      infos.background_parameter("B2", "background for the 2nd model", secondBackground);
-    }
 
-    return infos;
-  }
 
-  OutputInformation report_table(CompareModels.SimilarityInfo info) throws HashOverflowException {
-    OutputInformation infos = report_table_layout();
-    infos.add_resulting_value("S", "similarity", info.similarity());
-    infos.add_resulting_value("D", "distance (1-similarity)", info.distance());
-    infos.add_resulting_value("L", "length of the alignment", info.alignment.length());
-    infos.add_resulting_value("SH", "shift of the 2nd PWM relative to the 1st", info.alignment.shift());
-    infos.add_resulting_value("OR", "orientation of the 2nd PWM relative to the 1st", info.alignment.orientation());
-    infos.add_resulting_value("A1", "aligned 1st matrix", info.alignment.first_model_alignment());
-    infos.add_resulting_value("A2", "aligned 2nd matrix", info.alignment.second_model_alignment());
-    infos.add_resulting_value("W", "number of words recognized by both models (model = PWM + threshold)", info.recognizedByBoth );
-    infos.add_resulting_value("W1", "number of words and recognized by the first model", info.recognizedByFirst );
-    infos.add_resulting_value("P1", "P-value for the 1st matrix", info.realPvalueFirst(firstBackground));
-    if (predefinedFirstThreshold == null) {
-      infos.add_resulting_value("T1", "threshold for the 1st matrix", thresholdFirst() );
-    }
-    infos.add_resulting_value("W2", "number of words recognized by the 2nd model", info.recognizedBySecond );
-    infos.add_resulting_value("P2", "P-value for the 2nd matrix", info.realPvalueSecond(secondBackground));
-    if (predefinedSecondThreshold == null) {
-      infos.add_resulting_value("T2", "threshold for the 2nd matrix", thresholdSecond() );
-    }
-    return infos;
-  }
-
-  double thresholdFirst() throws HashOverflowException {
-    if (cacheFirstThreshold == null) {
-      if (predefinedFirstThreshold != null) {
-        cacheFirstThreshold = predefinedFirstThreshold;
-      } else {
-        CanFindThreshold pvalue_calculator = new FindThresholdAPE<PWM, BackgroundModel>(firstPWM, firstBackground, discretization, maxHashSize);
-        cacheFirstThreshold = pvalue_calculator.thresholdByPvalue(pvalue, pvalueBoundary).threshold;
-      }
-    }
-    return cacheFirstThreshold;
-  }
-
-  double thresholdSecond() throws HashOverflowException {
-    if (cacheSecondThreshold == null) {
-      if (predefinedSecondThreshold != null) {
-        cacheSecondThreshold = predefinedSecondThreshold;
-      } else {
-          CanFindThreshold pvalue_calculator = new FindThresholdAPE<PWM, BackgroundModel>(secondPWM, secondBackground, discretization, maxHashSize);
-        cacheSecondThreshold = pvalue_calculator.thresholdByPvalue(pvalue, pvalueBoundary).threshold;
-      }
-    }
-    return cacheSecondThreshold;
-  }
 
   OutputInformation report_table() throws Exception {
     CompareModels.SimilarityInfo results = calculator().jaccard(thresholdFirst(), thresholdSecond());
@@ -247,7 +100,7 @@ public class EvalSimilarity {
     } catch (Exception err) {
       System.err.println("\n" + err.getMessage() + "\n--------------------------------------\n");
       err.printStackTrace();
-      System.err.println("\n--------------------------------------\nUse --help option for help\n\n" + DOC);
+      System.err.println("\n--------------------------------------\nUse --help option for help\n\n" + new EvalSimilarity().documentString());
       System.exit(1);
     }
   }
---
+++ src/main/java/ru/autosome/perfectosape/cli/EvalSimilarityGeneralized.java
@@ -0,0 +1,200 @@
+package ru.autosome.perfectosape.cli;
+
+import ru.autosome.perfectosape.BoundaryType;
+import ru.autosome.perfectosape.backgroundModels.Background;
+import ru.autosome.perfectosape.backgroundModels.BackgroundModel;
+import ru.autosome.perfectosape.backgroundModels.GeneralizedBackgroundModel;
+import ru.autosome.perfectosape.calculations.CompareModels;
+import ru.autosome.perfectosape.calculations.HashOverflowException;
+import ru.autosome.perfectosape.calculations.findThreshold.CanFindThreshold;
+import ru.autosome.perfectosape.calculations.findThreshold.FindThresholdAPE;
+import ru.autosome.perfectosape.formatters.OutputInformation;
+import ru.autosome.perfectosape.importers.MotifImporter;
+import ru.autosome.perfectosape.importers.PMParser;
+import ru.autosome.perfectosape.motifModels.*;
+
+import java.util.ArrayList;
+
+public abstract class EvalSimilarityGeneralized <ModelType extends ScoringModel & Named & Discretable<ModelType> & ScoreDistribution<BackgroundType>,
+                                                 BackgroundType extends GeneralizedBackgroundModel> {
+  protected abstract String DOC_background_option();
+  protected abstract String DOC_run_string();
+
+  protected String documentString() {
+   return "Command-line format:\n" +
+    DOC_run_string() + " <1st matrix pat-file> <2nd matrix pat-file> [options]\n" +
+    "\n" +
+    "Options:\n" +
+    "  [-p <P-value>]\n" +
+    "  [-d <discretization level>]\n" +
+    "  [--pcm] - treat the input file as Position Count Matrix. PCM-to-PWM transformation to be done internally.\n" +
+    "  [--ppm] or [--pfm] - treat the input file as Position Frequency Matrix. PPM-to-PWM transformation to be done internally.\n" +
+    "  [--effective-count <count>] - effective samples set size for PPM-to-PWM conversion (default: 100). \n" +
+    "  [--boundary lower|upper] Upper boundary (default) means that the obtained P-value is greater than or equal to the requested P-value\n" +
+    "  [-b <background probabilities] " + DOC_background_option() + "\n" +
+    "  [--first-threshold <threshold for the first matrix>]\n" +
+    "  [--second-threshold <threshold for the second matrix>]\n" +
+    "\n" +
+    "Examples:\n" +
+    "  "+ DOC_run_string() + " motifs/KLF4_f2.pat motifs/SP1_f1.pat -p 0.0005 -d 100 -b 0.3,0.2,0.2,0.3\n";
+  }
+
+  protected BackgroundType firstBackground, secondBackground;
+  protected Double discretization;
+  protected double pvalue;
+  protected BoundaryType pvalueBoundary;
+  protected String firstPMFilename, secondPMFilename;
+  protected DataModel dataModelFirst, dataModelSecond;
+
+  protected Integer maxHashSize;
+  protected Integer maxPairHashSize;
+
+  protected Double effectiveCountFirst, effectiveCountSecond;
+
+  protected Double predefinedFirstThreshold, predefinedSecondThreshold;
+  protected ModelType firstPWM, secondPWM;
+
+  protected Double cacheFirstThreshold, cacheSecondThreshold;
+
+
+  protected abstract BackgroundType extract_background(String str);
+  protected abstract MotifImporter<ModelType> firstMotifImporter();
+  protected abstract MotifImporter<ModelType> secondMotifImporter();
+
+
+  void setup_from_arglist(ArrayList<String> argv) {
+    extract_first_pm_filename(argv);
+    extract_second_pm_filename(argv);
+    while (argv.size() > 0) {
+      extract_option(argv);
+    }
+    firstPWM = firstMotifImporter().loadPWMFromParser(PMParser.from_file_or_stdin(firstPMFilename));
+    secondPWM = secondMotifImporter().loadPWMFromParser(PMParser.from_file_or_stdin(secondPMFilename));
+  }
+
+  protected void extract_first_pm_filename(ArrayList<String> argv) {
+    if (argv.isEmpty()) {
+      throw new IllegalArgumentException("No input. You should specify input file");
+    }
+    firstPMFilename = argv.remove(0);
+  }
+
+  protected void extract_second_pm_filename(ArrayList<String> argv) {
+    if (argv.isEmpty()) {
+      throw new IllegalArgumentException("No input. You should specify input file");
+    }
+    secondPMFilename = argv.remove(0);
+  }
+
+  protected void extract_option(ArrayList<String> argv) {
+    String opt = argv.remove(0);
+    if (opt.equals("-b")) {
+      BackgroundType background = extract_background(argv.remove(0));
+      firstBackground = background;
+      secondBackground = background;
+    } else if (opt.equals("-p")) {
+      pvalue = Double.valueOf(argv.remove(0));
+    } else if (opt.equals("-b1")) {
+      firstBackground = extract_background(argv.remove(0));
+    } else if (opt.equals("-b2")) {
+      secondBackground = extract_background(argv.remove(0));
+    } else if (opt.equals("--max-hash-size")) {
+      maxHashSize = Integer.valueOf(argv.remove(0));
+    } else if (opt.equals("--max-2d-hash-size")) {
+      maxPairHashSize = Integer.valueOf(argv.remove(0));
+    } else if (opt.equals("-d")) {
+      discretization = Double.valueOf(argv.remove(0));
+    } else if (opt.equals("--boundary")) {
+      pvalueBoundary = BoundaryType.valueOf(argv.remove(0).toUpperCase());
+    } else if (opt.equals("--pcm")) {
+      dataModelFirst = DataModel.PCM;
+      dataModelSecond = DataModel.PCM;
+    } else if (opt.equals("--ppm") || opt.equals("--pfm")) {
+      dataModelFirst = DataModel.PPM;
+      dataModelSecond = DataModel.PPM;
+    } else if (opt.equals("--effective-count")) {
+      Double effectiveCount = Double.valueOf(argv.remove(0));
+      effectiveCountFirst = effectiveCount;
+      effectiveCountSecond = effectiveCount;
+    } else if (opt.equals("--first-threshold")) {
+      predefinedFirstThreshold = Double.valueOf(argv.remove(0));
+    } else if (opt.equals("--second-threshold")) {
+      predefinedSecondThreshold = Double.valueOf(argv.remove(0));
+    } else {
+      throw new IllegalArgumentException("Unknown option '" + opt + "'");
+    }
+  }
+
+  OutputInformation report_table_layout() {
+    OutputInformation infos = new OutputInformation();
+
+    infos.add_parameter("V", "discretization", discretization);
+    if (predefinedFirstThreshold == null || predefinedSecondThreshold == null) {
+      infos.add_parameter("P", "requested P-value", pvalue);
+    }
+    if (predefinedFirstThreshold != null) {
+      infos.add_parameter("T1", "threshold for the 1st matrix", predefinedFirstThreshold);
+    }
+    if (predefinedSecondThreshold != null) {
+      infos.add_parameter("T2", "threshold for the 2nd matrix", predefinedSecondThreshold);
+    }
+    infos.add_parameter("PB", "P-value boundary", pvalueBoundary);
+    if (firstBackground.equals(secondBackground)) {
+      infos.background_parameter("B", "background", firstBackground);
+    } else {
+      infos.background_parameter("B1", "background for the 1st model", firstBackground);
+      infos.background_parameter("B2", "background for the 2nd model", secondBackground);
+    }
+
+    return infos;
+  }
+
+  OutputInformation report_table(CompareModels.SimilarityInfo info) throws HashOverflowException {
+    OutputInformation infos = report_table_layout();
+    infos.add_resulting_value("S", "similarity", info.similarity());
+    infos.add_resulting_value("D", "distance (1-similarity)", info.distance());
+    infos.add_resulting_value("L", "length of the alignment", info.alignment.length());
+    infos.add_resulting_value("SH", "shift of the 2nd PWM relative to the 1st", info.alignment.shift());
+    infos.add_resulting_value("OR", "orientation of the 2nd PWM relative to the 1st", info.alignment.orientation());
+    infos.add_resulting_value("A1", "aligned 1st matrix", info.alignment.first_model_alignment());
+    infos.add_resulting_value("A2", "aligned 2nd matrix", info.alignment.second_model_alignment());
+    infos.add_resulting_value("W", "number of words recognized by both models (model = PWM + threshold)", info.recognizedByBoth );
+    infos.add_resulting_value("W1", "number of words and recognized by the first model", info.recognizedByFirst );
+    infos.add_resulting_value("P1", "P-value for the 1st matrix", info.realPvalueFirst(firstBackground));
+    if (predefinedFirstThreshold == null) {
+      infos.add_resulting_value("T1", "threshold for the 1st matrix", thresholdFirst() );
+    }
+    infos.add_resulting_value("W2", "number of words recognized by the 2nd model", info.recognizedBySecond );
+    infos.add_resulting_value("P2", "P-value for the 2nd matrix", info.realPvalueSecond(secondBackground));
+    if (predefinedSecondThreshold == null) {
+      infos.add_resulting_value("T2", "threshold for the 2nd matrix", thresholdSecond() );
+    }
+    return infos;
+  }
+
+  double thresholdFirst() throws HashOverflowException {
+    if (cacheFirstThreshold == null) {
+      if (predefinedFirstThreshold != null) {
+        cacheFirstThreshold = predefinedFirstThreshold;
+      } else {
+        CanFindThreshold pvalue_calculator = new FindThresholdAPE<ModelType, BackgroundType>(firstPWM, firstBackground, discretization, maxHashSize);
+        cacheFirstThreshold = pvalue_calculator.thresholdByPvalue(pvalue, pvalueBoundary).threshold;
+      }
+    }
+    return cacheFirstThreshold;
+  }
+
+  double thresholdSecond() throws HashOverflowException {
+    if (cacheSecondThreshold == null) {
+      if (predefinedSecondThreshold != null) {
+        cacheSecondThreshold = predefinedSecondThreshold;
+      } else {
+        CanFindThreshold pvalue_calculator = new FindThresholdAPE<ModelType, BackgroundType>(secondPWM, secondBackground, discretization, maxHashSize);
+        cacheSecondThreshold = pvalue_calculator.thresholdByPvalue(pvalue, pvalueBoundary).threshold;
+      }
+    }
+    return cacheSecondThreshold;
+  }
+
+
+}
