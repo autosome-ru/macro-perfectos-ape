ffb0eeeb76266dec403b45f59d353436b399a1eb
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-08-22 10:14:53 +0400

    DataModel.from_string

--- src/main/java/ru/autosome/commons/motifModel/types/DataModel.java
+++ src/main/java/ru/autosome/commons/motifModel/types/DataModel.java
@@ -1,5 +1,17 @@
 package ru.autosome.commons.motifModel.types;
 
 public enum DataModel {
-  PCM, PPM, PWM
+  PCM, PPM, PWM ;
+
+  public static DataModel fromString(String s) {
+    if (s.toUpperCase().equals("PWM")) {
+      return PWM;
+    } else if (s.toUpperCase().equals("PCM")) {
+      return PCM;
+    } else if (s.toUpperCase().equals("PPM")) {
+      return PPM;
+    } else {
+      throw new IllegalArgumentException("Unknown data model");
+    }
+  }
 }
