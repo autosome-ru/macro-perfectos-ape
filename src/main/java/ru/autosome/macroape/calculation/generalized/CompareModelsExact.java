d5972f41f4ae1165c1ac319e17d7a95292b6f8ef
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2017-05-30 22:23:07 +0300

    refactor

--- src/main/java/ru/autosome/macroape/calculation/generalized/CompareModelsExact.java
+++ src/main/java/ru/autosome/macroape/calculation/generalized/CompareModelsExact.java
@@ -8,15 +8,16 @@ import ru.autosome.macroape.model.ComparisonSimilarityInfo;
 import ru.autosome.macroape.model.PairAligned;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 
 public class CompareModelsExact<ModelType extends Alignable<ModelType>,
                                            BackgroundType extends GeneralizedBackgroundModel> {
-  public final ModelType firstPWM; // here we store discreted PWMs
-  public final ModelType secondPWM;
-  public final BackgroundType background;
-  public final Function<PairAligned<ModelType>, ? extends AlignedModelIntersection> calculatorOfAligned;
+  private final ModelType firstPWM; // here we store discreted PWMs
+  private final ModelType secondPWM;
+  private final BackgroundType background;
+  private final Function<PairAligned<ModelType>, ? extends AlignedModelIntersection> calculatorOfAligned;
 
   public CompareModelsExact(ModelType firstPWM, ModelType secondPWM,
                             BackgroundType background,
@@ -36,27 +37,18 @@ public class CompareModelsExact<ModelType extends Alignable<ModelType>,
     return result;
   }
 
-  protected double firstCountRenormMultiplier(PairAligned alignment) {
+  private double firstCountRenormMultiplier(PairAligned alignment) {
     return Math.pow(background.volume(), alignment.length() - firstPWM.length());
   }
-  protected double secondCountRenormMultiplier(PairAligned alignment) {
+  private double secondCountRenormMultiplier(PairAligned alignment) {
     return Math.pow(background.volume(), alignment.length() - secondPWM.length());
   }
 
   public ComparisonSimilarityInfo jaccard(double thresholdFirst, double thresholdSecond,
                                           double firstCount, double secondCount) {
-    double bestSimilarity = -1;
-    ComparisonSimilarityInfo bestSimilarityInfo = null;
-    for (Position position: relative_alignments()) {
-      ComparisonSimilarityInfo similarityInfo;
-      similarityInfo = jaccardAtPosition(thresholdFirst, thresholdSecond, firstCount, secondCount, position);
-      double similarity = similarityInfo.similarity();
-      if (similarity > bestSimilarity) {
-        bestSimilarity = similarity;
-        bestSimilarityInfo = similarityInfo;
-      }
-    }
-    return bestSimilarityInfo;
+    return relative_alignments().stream().map(
+        (Position position) -> jaccardAtPosition(thresholdFirst, thresholdSecond, firstCount, secondCount, position)
+    ).max(Comparator.comparingDouble(ComparisonSimilarityInfo::similarity)).get();
   }
 
   public ComparisonSimilarityInfo jaccardAtPosition(double thresholdFirst, double thresholdSecond,
